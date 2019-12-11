ff7362a00c282c453ee036be4f013319e84b7bfe
Preston Yadegar <preston.yadegar@gmail.com>
2019-12-11 18:01:02 -0500

    Add files via upload

---
+++ steam_analysis.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+
+
+data1 = pd.read_csv(filepath_or_buffer='~/Library/Preferences/PyCharmCE2018.3/scratches/steam-200k.csv')
+
+data2 = pd.read_csv(filepath_or_buffer='~/Library/Preferences/PyCharmCE2018.3/scratches/steam-store-games/steam.csv')
+
+print(data1.head(5))
+
+print(data2.head(5))
+
+
+
