e25a845f380b95bb3ed6b3f346f438e1042b24ee
Preston Yadegar <preston.yadegar@gmail.com>
2019-12-11 18:11:30 -0500

    Add files via upload

--- steam_analysis.py
+++ steam_analysis.py
@@ -2,14 +2,23 @@ import pandas as pd
 import numpy as np
 import matplotlib as plt
 
+# Makes the output display better for PyCharm. You can ignore elsewhere
+desired_width=320
+pd.set_option('display.width', desired_width)
+pd.set_option('display.max_columns',20)
 
-data1 = pd.read_csv(filepath_or_buffer='~/Library/Preferences/PyCharmCE2018.3/scratches/steam-200k.csv')
+# Change the filepaths to wherever you have them located
+data1 = pd.read_csv(filepath_or_buffer='~/Library/Preferences/PyCharmCE2018.3/scratches/steam-store-games/steam.csv')
+data2 = pd.read_csv(filepath_or_buffer='~/Library/Preferences/PyCharmCE2018.3/scratches/steam-200k.csv')
+# Column names were missing
+data2.columns = ['userid', 'name', 'behavior', 'hours', 'trashcol']
 
-data2 = pd.read_csv(filepath_or_buffer='~/Library/Preferences/PyCharmCE2018.3/scratches/steam-store-games/steam.csv')
+# print(data1.head(2))
+# print()
+# print(data2.head(2))
 
-print(data1.head(5))
-
-print(data2.head(5))
-
+# Not sure if this worked
+all_data = pd.merge(data1, data2, on='name')
+print(all_data.head(2))
 
 
